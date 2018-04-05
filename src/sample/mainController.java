5ea489a784439f4b6b9dbbfe5baa97c802155a52
lildavey <32276655+lildavey@users.noreply.github.com>
2018-04-04 23:02:11 -0500

    init

--- src/sample/mainController.java
+++ src/sample/mainController.java
@@ -1,10 +1,13 @@
 package sample;
 
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -13,6 +16,13 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** TODO: 4/4/2018
  * root directory
@@ -25,9 +35,11 @@ import java.io.IOException;
 public class mainController {
     @FXML private GridPane root;
     @FXML private TextField directorySearch, search;
+    @FXML private ListView fileList;
     @FXML private MenuButton searchType;
-    @FXML private Button directoryChooser;
+    @FXML private Button directoryChooser, searchBtn;
     @FXML private Stage stage;
+    private Stream<Path> fileArray;
 
     /**
      *
@@ -40,6 +52,7 @@ public class mainController {
     public void DirectorySearchAction(ActionEvent event) throws IOException
     {
         DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File("fileSearchTest"));
         File selectedDirectory = directoryChooser.showDialog(stage);
 
         if(selectedDirectory == null){
@@ -54,6 +67,18 @@ public class mainController {
     public void setStage(Stage stage) {
         this.stage = stage;
     }
+
+    public void searchBtnAction() throws Exception
+    {
+        List<File> filesInFolder = Files.walk(Paths.get(directorySearch.getText()))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+        ObservableList<File> list = FXCollections.observableArrayList(filesInFolder);
+        fileList.setItems(list);
+    }
+
 
 
 
