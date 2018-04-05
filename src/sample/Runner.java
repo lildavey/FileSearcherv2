1ad2c0c013c48ede445babe8d6ba14beed73ecc0
lildavey <32276655+lildavey@users.noreply.github.com>
2018-04-04 22:15:08 -0500

    Initial commit

---
+++ src/sample/Runner.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Runner extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = (Parent)loader.load();
+        mainController controller = (mainController) loader.getController();
+        controller.setStage(primaryStage);
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/mainController.java
@@ -0,0 +1,53 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+/** TODO: 4/4/2018
+ * root directory
+ * search term
+ * output list of files that match/contain search term
+ * save output to file
+ * status bar
+ * search text files
+ */
+public class mainController {
+    @FXML private GridPane root;
+    @FXML private TextField directorySearch, search;
+    @FXML private MenuButton searchType;
+    @FXML private Button directoryChooser;
+    @FXML private Stage stage;
+
+    @FXML
+    public void DirectorySearchAction(ActionEvent event) throws IOException
+    {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = directoryChooser.showDialog(stage);
+
+        if(selectedDirectory == null){
+            directorySearch.setText("No Directory selected");
+        }else{
+            directorySearch.setText(selectedDirectory.getAbsolutePath());
+        }
+
+
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+
+
+}
